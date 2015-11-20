2c2afe2e5a47f32e6d10867769590dab87c1280f
Judy <judy.programer@gmail.com>
2015-11-20 15:31:56 +0800

    update version.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ CLASSIFIERS = [
 
 setup(
     name='django-sendcloud',
-    version='0.2',
+    version='0.3',
     packages=['sendcloud'],
     author='jiaxin',
     author_email='jiaxin@guoku.com',
