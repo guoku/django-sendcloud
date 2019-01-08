c11f0c46c13cbd5853b914a6104fec09cddf47e7
jiaxin <edison7500@gmail.com>
2019-01-08 22:29:10 +0800

    update

--- sendcloud/conf.py
+++ sendcloud/conf.py
@@ -4,6 +4,7 @@ BASE_URL = "http://api.sendcloud.net/apiv2"
 SEND_CLOUD = {
     "send_mail": "{base_url}/mail/send".format(base_url=BASE_URL),
     "sendtemplate": "{base_url}/mail/sendtemplate".format(base_url=BASE_URL),
+    "mail_template_list": "{base_url}/template/list".format(base_url=BASE_URL),
 }
 
 send_cloud_config = SEND_CLOUD.copy()
@@ -14,6 +15,9 @@ def get_send_cloud_setting(settings):
 
 
 def get_send_mail_url():
-
     return get_send_cloud_setting('send_mail')
+
+
+def get_template_list():
+    return get_send_cloud_setting("mail_template_list")
 
