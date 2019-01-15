0c0ce415d5ac758b51acd84a06681ffb8f54e0d3
jiaxin <edison7500@gmail.com>
2019-01-15 14:23:31 +0800

    update

--- sendcloud/conf.py
+++ sendcloud/conf.py
@@ -19,6 +19,14 @@ SEND_CLOUD = {
         "update": "{base_url}/template/update".format(base_url=BASE_URL),
     },
 
+    "member": {
+        "list": "{base_url}/addressmember/list".format(base_url=BASE_URL),
+        "get": "{base_url}/addressmember/get".format(base_url=BASE_URL),
+        "add": "{base_url}/addressmember/add".format(base_url=BASE_URL),
+        "update": "{base_url}/addressmember/update".format(base_url=BASE_URL),
+        "delete": "{base_url}/addressmember/delete".format(base_url=BASE_URL),
+    },
+
     "mail_template_list": "{base_url}/template/list".format(base_url=BASE_URL),
     "mail_template_get": "{base_url}/template/get".format(base_url=BASE_URL),
     "mail_template_add": "{base_url}/template/add".format(base_url=BASE_URL),
@@ -62,27 +70,31 @@ def get_task_info():
 
 
 # ----------------------------------------------------------------------------------------------------------------------
+def get_template_config(kind="list"):
+    _template = get_send_cloud_setting("template")
+    return _template.get(kind)
+
 
 def get_template_list():
-    return get_send_cloud_setting("mail_template_list")
+    return get_template_config()
 
 
 def get_template():
-    return get_send_cloud_setting("mail_template_get")
+    return get_template_config(kind='get')
 
 
 def add_template():
-    return get_send_cloud_setting("mail_template_add")
+    return get_template_config(kind="add")
 
 
 def delete_template():
-    return get_send_cloud_setting("mail_template_delete")
+    return get_template_config("delete")
 
 
 def update_template():
-    return get_send_cloud_setting("mail_template_update")
+    return get_template_config("update")
 
-
+# ----------------------------------------------------------------------------------------------------------------------
 def member_list():
     return get_send_cloud_setting("member_list")
 
