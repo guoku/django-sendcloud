a28218961f1af91b58a443aa0b9c4b6fe6a33ea2
jiaxin <edison7500@gmail.com>
2019-10-08 15:52:06 +0800

    run block

--- setup.py
+++ setup.py
@@ -1,40 +1,40 @@
 from setuptools import setup
-import sendcloud
+from sendcloud import version
 
 DESCRIPTION = "A Django email backend for SendCloud"
 
 LONG_DESCRIPTION = None
 
-VERSION = sendcloud.__version__
+VERSION = version.__version__
 
 try:
-    LONG_DESCRIPTION = open('README.md').read()
+    LONG_DESCRIPTION = open("README.md").read()
 except:
     pass
 
 
-install_requires = (["requests>=2.1",],)
+install_requires = (["requests>=2.1"],)
 
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Framework :: Django',
-    'Framework :: Django :: 1.11',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3.6',
-    'Topic :: Software Development :: Libraries :: Python Modules',
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Framework :: Django",
+    "Framework :: Django :: 1.11",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 
 setup(
-    name='django-sendcloud',
+    name="django-sendcloud",
     version=VERSION,
-    packages=['sendcloud'],
-    author='jiaxin',
-    author_email='edison7500@gmail.com',
-    url='https://github.com/edison7500/django-sendcloud',
-    license='MIT',
+    packages=["sendcloud"],
+    author="jiaxin",
+    author_email="edison7500@gmail.com",
+    url="https://github.com/edison7500/django-sendcloud",
+    license="MIT",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     # platforms=['any'],
@@ -44,4 +44,4 @@ setup(
     zip_safe=False,
 )
 
-__author__ = 'edison7500'
+__author__ = "edison7500"
